99ac8cc47055f04d05abc0b1a99533721c29820b
Luphia Chang <luphia@outlook.com>
2015-11-03 18:44:31 +0800

    Update connection

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var udp2p = require('udp2p');
 var udp2p = require('./index.js');
 client = new udp2p();
 
-var server = { host: '127.0.0.1', port: 6312 };
+var server = { address: '127.0.0.1', port: 2266 };
 client.connect(server);
-var list = client.getClientList();
+client.getClientList(function(e, d) { console.log(JSON.stringify(d)); });
 client.peerTo(list[1], function () {});
 
 var msg = { test: 'message' };
@@ -17,17 +17,16 @@ client.send(msg, list[1], function () {});
 var os = require('os'),
     dgram = require('dgram'),
     net = require('net'),
+    raid2x = require('raid2x'),
     dvalue = require('dvalue');
+
+var server = {
+  address: 'laria.space',
+  port: 2266
+};
 
 var udp2p = function (config) {
   this.init(config);
-};
-
-udp2p.guid = function () {
-	var s4 = function() {
-		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-	};
-	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +s4() + '-' + s4() + s4() + s4();
 };
 
 // get self IP for connection
@@ -65,7 +64,7 @@ udp2p.fetchIP = function (cb) {
       IPs.push(ip);
     }
 
-    cb(undefined, { host: IPs });
+    cb(undefined, { address: IPs });
   });
 };
 
@@ -103,12 +102,19 @@ udp2p.fetchInfo = function (cb) {
 udp2p.prototype.init = function (config) {
   var self = this;
   this.info = {};
+  this.clients = [];
+  this.clientIndex = {};
+
+  this.waiting = {};
+  this.result = {};
+  this.callback = {};
+
   this.isReady = false;
   this.isStart = false;
 
   config = dvalue.default(config, {
       server: false,
-      name: udp2p.guid()
+      name: dvalue.guid()
   });
 
   udp2p.fetchInfo(function (err, data) {
@@ -179,29 +185,45 @@ udp2p.prototype.getStatus = function () {
 };
 
 udp2p.prototype.exeMessage = function (msg, peer) {
-  switch (msg.type) {
-    case 'punch':
-      var cmd = { type: 'ack' };
-      var message = this.translate(cmd);
-      this.send(message, peer);
-      break;
-    case 'connect':
-      break;
-    default:
+  try {
+    msg = JSON.parse(msg);
+    switch (msg.type) {
+      case 'clientList':
+        this.done(msg._id, msg.clientList);
+        break;
+      case 'punch':
+        var message = this.translate('ack');
+        this.send(message, peer);
+        break;
+      case 'connection':
+        var node = msg.client
+        break;
+      default:
+    }
+  }
+  catch (err) {
+    // recieve file buffer
   }
 };
 
 udp2p.prototype.translate = function (cmd) {
   if(typeof(cmd) != 'object') { cmd = { type: cmd }; }
-  var message = {};
+  var message = { _id: dvalue.randomID() };
   switch (cmd.type) {
     case 'register':
       message.type = 'register';
       message.name = this.config.name;
-      message.node = {
-        host: this.info.host[0],
+      message.linfo = {
+        address: this.info.address[0],
         port: this.info.port
       };
+      break;
+    case 'clientList':
+      message.type = 'clientList';
+      break;
+    case 'punch':
+      message.type = 'punch';
+      message.from = this.config.name;
       break;
     case 'ack':
       break;
@@ -217,21 +239,107 @@ udp2p.prototype.regist = function (server, cb) {
 };
 
 // need to link to tracker first
-udp2p.prototype.connect = function (server, cb) {
+udp2p.prototype.connect = function (node, cb) {
   var self = this;
+  server = dvalue.default(node, server);
+
   this.start(function() {
     var msg = self.translate('register');
     self.send(msg, server, function() {});
   });
 };
 
-udp2p.prototype.peerTo = function (client, cb) {
+udp2p.prototype.getClientList = function (cb) {
+  cb = dvalue.default(cb, function () {});
+  var self = this;
+  var msg = self.translate('clientList');
+  this.addJob(msg._id, 1, function (err, data) {
+      data.map(function (v) {
+        self.addClient(v);
+      });
+      cb(undefined, self.clients);
+  });
+  this.send(msg, server, function () {});
+};
 
+udp2p.prototype.addClient = function (client) {
+  client = dvalue.default(client, {
+    "name": undefined,
+    "connections": {
+      "local": {
+        "address": undefined,
+        "port": undefined
+      },
+      "public": {
+        "address": undefined,
+        "family": undefined,
+        "port": undefined
+      }
+    }
+  });
+  if(!this.clientIndex[client.name]) {
+    this.clientIndex[client.name] = (this.clients.push(client) - 1);
+  }
+  return this.clientIndex[client.name]
+};
+
+udp2p.prototype.peerTo = function (client, cb) {
+  var self = this;
+  var msg = self.translate('connect');
+  self.send(msg, server, function() {});
 };
 
 udp2p.prototype.send = function (msg, peer, cb) {
   var data = new Buffer(JSON.stringify(msg));
-  this.udp.send(data, 0, data.length, peer.port, peer.host, function(err, bytes) {});
+  this.udp.send(data, 0, data.length, peer.port, peer.address, function(err, bytes) {});
+};
+
+udp2p.prototype.initEvent = function(event) {
+	if(!event) { event = "_job"; }
+	this.waiting[event] = 0;
+	this.result[event] = [];
+};
+udp2p.prototype.addJob = function(event, n, callback) {
+	if(!event) { event = "_job"; }
+	if(!this.waiting[event]) { this.initEvent(event); }
+
+	this.waiting[event] += n > 0? n: 1;
+
+	if(typeof(callback) == 'function') {
+		this.callback[event] = callback;
+	}
+};
+udp2p.prototype.done = function(event, data) {
+	if(!event) { event = "_job"; }
+	if(!this.waiting[event]) { return false; }
+	if(data) { this.result[event].push(data); }
+
+	this.waiting[event] --;
+	this.waiting[event] = this.waiting[event] < 0? 0: this.waiting[event];
+
+	if(this.waiting[event] == 0) {
+		if(this.result[event].length == 1) { this.result[event] = this.result[event][0]; }
+		this.cbReturn(false, this.result[event], this.callback[event]);
+		this.cleanEvent(event);
+	}
+};
+udp2p.prototype.cleanEvent = function(event) {
+	if(!event) { event = "_job"; }
+	if(!this.waiting[event]) { return false; }
+
+	delete this.result[event];
+	delete this.waiting[event];
+	return true;
+};
+udp2p.prototype.cbReturn = function(err, data, callback) {
+	if(typeof(callback) != 'function') {
+		callback = function(err, data) {
+			// err && (console.log(err));
+			// data && (console.log(data));
+		};
+	}
+
+	callback(err, data);
 };
 
 module.exports = udp2p;
